9a2e75635d01efbe40449f813e8c2cfeb1b7fdb4
tronico <tronico@o2online.de>
2016-04-13 17:04:20 +0200

    U1

--- Uebung_1/public_html/js/app.js
+++ Uebung_1/public_html/js/app.js
@@ -1,7 +1,28 @@
 /**
  * Created on : 07.04.2016
- * Authors    : Loki, Tronico, X3phiroth
+ * Authors    : Lisa Bitterling, Nico Nauendorf, Christoph Kozielski
  */
 
-test
+// startet die Funktion init wenn DOM vollständig geladen wurde
+window.addEventListener("load",init, false);
 
+// manipuliert jedes Videoelement
+function init(){
+    var allVideoContainer = document.getElementsByTagName("video");
+    
+    for(var i = 0; i < allVideoContainer.length; i++){
+        containerBuild(allVideoContainer[i]);
+    }
+}
+
+// fügt jedem Videoelement Interaktionsbutton hinzu
+function containerBuild(videoContainer){
+    console.log(videoContainer);
+    //videoContainer.insertAdjacentHTML('beforebegin', '<section>');
+    //videoContainer.insertAdjacentHTML('afterend', 'cda</section>');
+}
+
+// meldet Eventhändler an
+function buttonEvent(button){
+    
+}
