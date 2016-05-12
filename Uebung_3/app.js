97b5ac491bbac0a7cac2fbe88b70929c4d9f9c9d
LeeShaw <l.bitterling@outlook.de>
2016-05-12 13:54:55 +0200

    aufgabe 1, review final

--- Uebung_3/app.js
+++ Uebung_3/app.js
@@ -11,7 +11,7 @@
  * @licence CC BY-SA 4.0
  *
  */
-"use strict";  // tell node.js to be more "strict" in JavaScript parsing (e.g. not allow variables without var before)
+"use strict"; // tell node.js to be more "strict" in JavaScript parsing (e.g. not allow variables without var before)
 
 // node module imports
 var path = require('path');
@@ -27,16 +27,15 @@ var app = express();
 // Middleware ************************************
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.urlencoded({extended: false}));
 // logging
-app.use(function(req, res, next) {
-    console.log('Request of type '+req.method + ' to URL ' + req.originalUrl);
+app.use(function (req, res, next) {
+    console.log('Request of type ' + req.method + ' to URL ' + req.originalUrl);
     next();
 });
 
 // API-Version control. We use HTTP Header field Accept-Version instead of URL-part /v1/
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     // expect the Accept-Version header to be NOT set or being 1.0
     var versionWanted = req.get('Accept-Version');
     if (versionWanted !== undefined && versionWanted !== '1.0') {
@@ -48,11 +47,11 @@ app.use(function(req, res, next){
 });
 
 // request type application/json check
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     if (['POST', 'PUT'].indexOf(req.method) > -1 &&
-        !( /application\/json/.test(req.get('Content-Type')) )) {
+            !(/application\/json/.test(req.get('Content-Type')))) {
         // send error code 415: unsupported media type
-        res.status(415).send('wrong Content-Type');  // user has SEND the wrong type
+        res.status(415).send('wrong Content-Type'); // user has SEND the wrong type
     } else if (!req.accepts('json')) {
         // send 406 that response will be application/json and request does not support it by now as answer
         // user has REQUESTED the wrong type
@@ -63,42 +62,57 @@ app.use(function(req, res, next) {
     }
 });
 
+// Routes  TWEETS ***************************************
 
-// Routes ***************************************
-
-app.get('/tweets', function(req,res,next) {
+app.get('/tweets', function (req, res, next) {
     res.json(store.select('tweets'));
 });
-
-app.post('/tweets', function(req,res,next) {
-    var id = store.insert('tweets', req.body); 
+app.post('/tweets', function (req, res, next) {
+    var id = store.insert('tweets', req.body);
     // set code 201 "created" and send the item back
     res.status(201).json(store.select('tweets', id));
 });
-
-
-app.get('/tweets/:id', function(req,res,next) {
+app.get('/tweets/:id', function (req, res, next) {
     res.json(store.select('tweets', req.params.id));
 });
-
-app.delete('/tweets/:id', function(req,res,next) {
+app.delete('/tweets/:id', function (req, res, next) {
     store.remove('tweets', req.params.id);
     res.status(200).end();
 });
-
-app.put('/tweets/:id', function(req,res,next) {
+app.put('/tweets/:id', function (req, res, next) {
     store.replace('tweets', req.params.id, req.body);
     res.status(200).end();
 });
 
 
-// TODO: add your routes etc.
+// Routes  USER ***************************************     
 
+app.route('/users')
+        .get(function (req, res, next) {
+            res.json(store.select('users'));
+        })
+        .post(function (req, res, next) {
+            var id = store.insert('users', req.body);
+            // set code 201 "created" and send the item back
+            res.status(201).json(store.select('users', id));
+        });
 
+app.route('/users/:id')
+        .get(function (req, res, next) {
+            res.json(store.select('users', req.params.id));
+        })
+        .delete(function (req, res, next) {
+            store.remove('users', req.params.id);
+            res.status(200).end();
+        })
+        .put(function (req, res, next) {
+            store.replace('users', req.params.id, req.body);
+            res.status(200).end();
+        });
+        
 // CatchAll for the rest (unfound routes/resources ********
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -109,7 +123,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace as JSON response
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         console.log('Internal Error: ', err.stack);
         res.status(err.status || 500);
         res.json({
@@ -123,7 +137,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
         error: {
@@ -135,9 +149,9 @@ app.use(function(err, req, res, next) {
 
 
 // Start server ****************************
-app.listen(3000, function(err) {
+app.listen(3000, function (err) {
     if (err !== undefined) {
-        console.log('Error on startup, ',err);
+        console.log('Error on startup, ', err);
     }
     else {
         console.log('Listening on port 3000');
