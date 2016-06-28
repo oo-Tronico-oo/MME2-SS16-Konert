92839651dbc5ef42fc28f21c3faab41368123f9f
oo-Tronico-oo <tronico@o2online.de>
2016-06-28 16:04:32 +0200

    U6 A1a finish

--- Uebung_6/public/js/appclient.js
+++ Uebung_6/public/js/appclient.js
@@ -22,45 +22,21 @@ requirejs.config({
 });
 
 // AMD conform require as provided by require.js
-require(['jquery','backbone', 'models/tweet', 'models/user', 'views/user', 'views/tweet-list', 'views/tweet-create'],
-        function($, Backbone, Tweet, User, UserView, TweetListView, TweetCreateView) {
+require(['jquery', 'backbone'],
+        function ($, Backbone) {
 
-    var AppRouter = Backbone.Router.extend({
-        routes: {
-            '': 'main',
-            '*whatever': 'main'
-        },
-        main: function(){
-            var user = new User.Model({_id : '57554efd6c5edc1f5c9a2778'}); // FIXME add correct user id here
-                                                                            // hard coded instead of login..
-            var userView = new UserView({model: user});
-            var that = this;
-            user.fetch( {
-               error: function(model, response) {
-                   console.error("no user fetched");
-                   userView.render();
-               },
-                success: function(model, response) {
-                   userView.render();
-                    that.user = user;
+            var AppRouter = Backbone.Router.extend({
+                routes: {
+                    '': 'main',
+                    '*whatever': 'main'
+                },
+                main: function () {
+                    $('body').prepend('<h1>Video App</h1>');
                 }
             });
 
-            var tweets = new Tweet.Collection();
-            var tweetListView = new TweetListView({collection: tweets});
-            tweets.fetch({
-                error: function (model, response) {
-                    console.error("no user fetched");
-                    userView.render();
-                },
-                success: tweetListView.render
-            });
-            var tweetCreateView = new TweetCreateView({collection:tweets, app: this});
-        }
-    });
+            var myRouter = new AppRouter();
 
-    var myRouter = new AppRouter();
-
-    // finally start tracking URLs to make it a SinglePageApp (not really needed at the moment)
-    Backbone.history.start({pushState: true}); // use new fancy URL Route mapping without #
-});
+            // finally start tracking URLs to make it a SinglePageApp (not really needed at the moment)
+            Backbone.history.start({pushState: true}); // use new fancy URL Route mapping without #
+        });
